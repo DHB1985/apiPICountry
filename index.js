5bd95108b6aa7efa4f45ccf9675ce78a95099d5a
Daniel <danielbarbieri@gmail.com>
2023-02-13 18:08:43 -0300

    add/new db

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const {startServer} = require('./src/startserver.js')
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT || 3001, async () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log('Listening'); // eslint-disable-line no-console
     await startServer();
     console.log('Carga Completa')
   });
